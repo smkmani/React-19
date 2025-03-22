0e86d0acc7c2e986df67d706596e557c644cc066
Sai Mithran <mani.beddy@gmail.com>
2025-03-23 01:00:48 +0530

    Admin Dash board init

--- src/components/common/types.tsx
+++
@@ -1,4 +0,0 @@
-import { JSX, ReactNode } from 'react';
-
-export type ChildrenProps = string | JSX.Element | JSX.Element[];
-export type PropsWithChildren<P> = P & { children?: ReactNode };
--- src/components/layout/AppLayout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-const AppLayout = () => {
-  return <Outlet />;
-};
-
-export default AppLayout;
